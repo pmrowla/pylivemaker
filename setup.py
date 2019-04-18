31548027995fce7e9e655a4e885ab73474048213
Peter Rowlands <peter@pmrowla.com>
2019-04-18 15:22:50 +0900

    Bump version: 0.1.1 → 0.1.2-dev [ci skip]

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/pmrowla/pylivemaker',
-    version='0.1.1',
+    version='0.1.2-dev',
     zip_safe=False,
 )
