0198b700e2febb4ef52e34efa237207b8e7b213a
Peter Rowlands <peter@pmrowla.com>
2019-08-15 18:46:58 +0900

    [lmpatch] support patching split archives
    - see #12
    - when input archive ends with .ext file extension, output archive will
      be split
    - for .dat archives without separate .ext index file, --split option
      must be specified
    - patching standalone .exe files will never generate split output
      (need example to verify that this case is even possible in LM)

--- livemaker/patch.py
+++ livemaker/patch.py
@@ -20,6 +20,7 @@
 
 import logging
 import os
+import os.path
 import sys
 import tempfile
 import shutil
@@ -39,25 +40,35 @@ log.addHandler(fh)
 
 @click.command()
 # @lmlsb.option('-r', '--recursive', is_flag=True, default=False)
-@click.argument('exe_file', required=True, type=click.Path(exists=True, dir_okay=False))
+@click.argument('archive_file', required=True, type=click.Path(exists=True, dir_okay=False))
 @click.argument('patched_lsb', required=True, type=click.Path(exists=True, dir_okay=False))
-def lmpatch(exe_file, patched_lsb):
+@click.option('--split', is_flag=True, default=False,
+              help='Generate a split data archive.')
+@click.option('--no-backup', is_flag=True, default=False,
+              help='Do not generate backup of original archive file(s).')
+@click.option('-f', '--force', is_flag=True, default=False,
+              help='Overwrite any existing files instead of erroring out.')
+def lmpatch(archive_file, patched_lsb, split, no_backup, force):
     """Patch a LiveMaker game.
 
     Any existing version of patched_lsb will be replaced in the specified
-    LiveMaker executable. If a file with the same name as patched_lsb does
+    LiveMaker archive. If a file with the same name as patched_lsb does
     not already exist, this will do nothing.
 
-    A backup copy of the old exe will also be created.
+    If file extension for archive_file is ".ext", or if archive_file is not
+    an excutable and the --split option is specified,
+    a split archive will be generated.
+
+    A backup copy of the old archive file(s) will also be created unless the
+    --no-backup option is specified.
 
     """
-    exe_path = Path(exe_file)
-    backup_path = Path('{}.bak'.format(exe_path))
-    if backup_path.exists():
-        sys.exit('{} already exists'.format(backup_path))
+    archive_path = Path(archive_file).resolve()
+    archive_dir = archive_path.parent
+    archive_name = archive_path.name
 
     try:
-        orig_lm = LMArchive(exe_path)
+        orig_lm = LMArchive(archive_path)
     except LiveMakerException as e:
         log.error(e)
         return
@@ -68,16 +79,31 @@ def lmpatch(exe_file, patched_lsb):
         fp.write(orig_lm.read_exe())
         fp.close()
     else:
+        if orig_lm.is_split:
+            split = True
         tmp_exe = None
+
+    if not no_backup:
+        backup_paths = {archive_path: Path('{}.bak'.format(archive_path))}
+        if orig_lm.is_split:
+            for p in orig_lm._split_files:
+                backup_paths[Path(p)] = Path('{}.bak'.format(p))
+        for p in backup_paths.values():
+            if p.exists():
+                if force:
+                    print('{} will be overwritten'.format(p))
+                else:
+                    sys.exit('{} already exists'.format(p))
 
     lsb_path = PureWindowsPath(patched_lsb)
     try:
-        fd, tmpfile_path = tempfile.mkstemp()
-        tmpfile_path = Path(tmpfile_path)
-        tmpfp = os.fdopen(fd, 'wb')
-        log.info('opened {}'.format(tmpfile_path))
-        print('Writing archive contents')
-        with LMArchive(exe_path, 'w', fp=tmpfp, version=orig_lm.version, exe=tmp_exe) as new_lm:
+        tmpdir = tempfile.mkdtemp()
+        tmpdir_path = Path(tmpdir)
+        log.info('Using temp directory {}'.format(tmpdir_path))
+        print('Generating new archive contents...')
+        with LMArchive(name=tmpdir_path.joinpath(archive_name), mode='w', version=orig_lm.version, exe=tmp_exe,
+                       split=split) as new_lm:
+
             def bar_show(item):
                 width, _ = click.get_terminal_size()
                 width //= 4
@@ -85,15 +111,14 @@ def lmpatch(exe_file, patched_lsb):
                 if len(name) > width:
                     name = ''.join(['...', name[-width:]])
                 return name
+
             # patch
             with click.progressbar(orig_lm.infolist(), item_show_func=bar_show) as bar:
                 for info in bar:
-                    # data = orig_lm.read(info, decompress=False)
-                    # new_lm.writebytes(info, data)
                     if info.path == lsb_path:
                         # replace existing with patch version, use original
                         # compress type
-                        new_lm.write(lsb_path, compress_type=info.compress_type)
+                        new_lm.write(lsb_path, compress_type=info.compress_type, unk1=info.unk1)
                         log.info('patched {}'.format(lsb_path))
                         # print('patched')
                     else:
@@ -103,24 +128,34 @@ def lmpatch(exe_file, patched_lsb):
                         log.info('copied {}'.format(lsb_path))
                     # print(info.name)
         orig_lm.close()
-        tmpfp.close()
 
-        # backup original file
-        exe_path.rename(backup_path)
-        # copy tmpfile to output path then remove the tmp copy.
+        # copy temp dir contents to output path then remove the temp dir
         # this operation needs to be a copy instead of rename (move)
         # in case windows system temp directory is on a different
         # logical drive than the output path
-        shutil.copy(tmpfile_path, exe_path)
-        tmpfile_path.unlink()
+        print('Writing new archive files...')
+        for root, dirs, files in os.walk(tmpdir_path):
+            for name in files:
+                tmp_p = Path(root).joinpath(name)
+                orig_p = archive_dir.joinpath(name)
+                if orig_p.exists() and not no_backup:
+                    orig_p.rename(backup_paths[orig_p])
+                shutil.copy(tmp_p, archive_dir)
     except Exception as e:
         log.error(e)
-        # remove
-        tmpfile_path.unlink()
+
         raise e
     finally:
         if tmp_exe is not None:
             Path(tmp_exe).unlink()
+
+        print('Cleaning up temporary files...')
+        if tmpdir_path:
+            for root, dirs, files in os.walk(tmpdir_path):
+                for name in files:
+                    p = Path(root).joinpath(name)
+                    p.unlink()
+            tmpdir_path.rmdir()
 
 
 if __name__ == '__main__':
