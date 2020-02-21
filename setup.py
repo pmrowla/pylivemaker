41420fb9191680fcddf773a62fad28bc298d74f7
Peter Rowlands <peter@pmrowla.com>
2020-02-21 13:45:31 +0900

    remove old py ver from setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
