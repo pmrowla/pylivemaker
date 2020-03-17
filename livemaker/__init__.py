775365e35652c0b6130e9b7474d13c2a5bfe17b8
Peter Rowlands <peter@pmrowla.com>
2020-03-17 16:02:18 +0900

    Bumpversion: 0.2.2->0.2.3-dev [ci skip]

--- livemaker/__init__.py
+++ livemaker/__init__.py
@@ -21,7 +21,7 @@
 __author__ = """Peter Rowlands"""
 __email__ = 'peter@pmrowla.com'
 __license__ = 'GPLv3'
-__version__ = '0.2.2'
+__version__ = '0.2.3-dev'
 
 
 from .archive import LMArchive, LMArchiveInfo, LMCompressType
--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/pmrowla/pylivemaker',
-    version='0.2.2',
+    version='0.2.3-dev',
     zip_safe=False,
 )
