50526e95b3828adab7372cc8cf6f4d977bc02f1b
Peter Rowlands <peter@pmrowla.com>
2020-07-01 10:57:55 +0900

    Bump version 1.0.0 -> 1.0.1-dev

--- livemaker/__init__.py
+++ livemaker/__init__.py
@@ -21,7 +21,7 @@
 __author__ = """Peter Rowlands"""
 __email__ = "peter@pmrowla.com"
 __license__ = "GPLv3"
-__version__ = "1.0.0"
+__version__ = "1.0.1-dev"
 
 
 from loguru import logger
--- setup.py
+++ setup.py
@@ -65,6 +65,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/pmrowla/pylivemaker",
-    version="1.0.0",
+    version="1.0.1-dev",
     zip_safe=False,
 )
