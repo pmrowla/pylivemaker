e537b32c0b6b00338cb17e1c392abf23f870d43c
Peter Rowlands <peter@pmrowla.com>
2023-01-01 13:43:44 +0900

    release 1.0.4

--- setup.py
+++ setup.py
@@ -65,6 +65,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/pmrowla/pylivemaker",
-    version="1.0.4-dev",
+    version="1.0.4",
     zip_safe=False,
 )
